d71776dfec20afcc966d1cecf44d329f0922e7fe
Aaishika S Bhattacharya <aaishikasb@users.noreply.github.com>
2020-06-13 04:37:32 +0530

    Remove Files

--- scripts/navbar_h.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import React from 'react';
-import htmlContent from 'Web%20Components/NavBar.html';
-export default function navbar_h() {
-    return (
-        <div dangerouslySetInnerHTML={ {__html: htmlContent} } />
-    );
-}
